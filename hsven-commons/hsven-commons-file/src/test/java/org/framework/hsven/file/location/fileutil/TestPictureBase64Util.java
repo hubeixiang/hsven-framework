5252cc49d965c7ab938b72afffbff74c5c7d91ae
xiangxujian <hubeixiang@aliyun.com>
2020-05-25 13:54:24 +0800

    CommonPath util add

--- hsven-commons/hsven-commons-file/src/test/java/org/framework/hsven/file/location/fileutil/TestPictureBase64Util.java
+++ hsven-commons/hsven-commons-file/src/test/java/org/framework/hsven/file/location/fileutil/TestPictureBase64Util.java
@@ -2,15 +2,21 @@ package org.framework.hsven.file.location.fileutil;
 
 import org.junit.Test;
 
+import java.util.List;
+
 
 public class TestPictureBase64Util {
 
     @Test
     public void testBase64() {
+        String filepath = "D:/03Downloads/Browser/zrs-a-nav-config-file.define";
+        List list = SimpleFileUtil.read2ListString(filepath);
+        System.out.println(list);
+
         String path = "D:\\02xStaff\\image\\";
         String oriFile = "儿子的画.jpg";
         String imageContext = PictureBase64Util.generateBase64Context(path + oriFile);
-        System.out.println(imageContext);
+//        System.out.println(imageContext);
         PictureBase64Util.generateImageFile(imageContext, path, "xxx.jpg");
     }
 }
