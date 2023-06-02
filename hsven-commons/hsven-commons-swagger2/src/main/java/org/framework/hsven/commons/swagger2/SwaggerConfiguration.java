868e1c7d5a17cc4f205257539652942ccba59f9a
xiangxujian <hubeixiang@aliyun.com>
2023-06-02 18:17:25 +0800

    spring-boot 升级到2.7.10;同时适配springfox-swagger2版本3.0.0的修改

--- hsven-commons/hsven-commons-swagger2/src/main/java/org/framework/hsven/commons/swagger2/SwaggerConfiguration.java
+++ hsven-commons/hsven-commons-swagger2/src/main/java/org/framework/hsven/commons/swagger2/SwaggerConfiguration.java
@@ -1,9 +1,14 @@
 package org.framework.hsven.commons.swagger2;
 
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -11,7 +16,13 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spring.web.plugins.WebFluxRequestHandlerProvider;
+import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ConditionalOnMissingBean(SwaggerConfiguration.class)
 @Configuration
@@ -52,4 +63,38 @@ public class SwaggerConfiguration {
                 .build();
     }
 
+    @ConditionalOnProperty(prefix = "swagger.springfox.fixed", name = "enable", havingValue = "true", matchIfMissing = true)
+    @Bean
+    public BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
+        return new BeanPostProcessor() {
+            @Override
+            public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+                if (bean instanceof WebMvcRequestHandlerProvider
+                        || bean instanceof WebFluxRequestHandlerProvider) {
+                    customRequestMappingHandlerMapping(getHandlerMapping(bean));
+                }
+                return bean;
+            }
+
+            private <T extends RequestMappingInfoHandlerMapping> void customRequestMappingHandlerMapping(List<T> mappings) {
+                List<T> copy = mappings.stream()
+                        .filter(mapping -> mapping.getPatternParser() == null)
+                        .collect(Collectors.toList());
+                mappings.clear();
+                mappings.addAll(copy);
+            }
+
+            @SuppressWarnings("unchecked")
+            private List<RequestMappingInfoHandlerMapping> getHandlerMapping(Object bean) {
+                try {
+                    Field field = ReflectionUtils.findField(bean.getClass(), "handlerMappings");
+                    field.setAccessible(true);
+                    return (List<RequestMappingInfoHandlerMapping>) field.get(bean);
+                } catch (IllegalArgumentException | IllegalAccessException e) {
+                    throw new IllegalStateException(e);
+
+                }
+            }
+        };
+    }
 }
